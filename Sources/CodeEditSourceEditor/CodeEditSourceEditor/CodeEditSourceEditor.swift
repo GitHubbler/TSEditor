d441cce03811f437258619708a0b83cae07872ee
Pierre van Aswegen <pierre@preceptorsoft.com>
2025-03-09 16:19:06 -0700

    dispatch with delay

--- Sources/CodeEditSourceEditor/CodeEditSourceEditor/CodeEditSourceEditor.swift
+++ Sources/CodeEditSourceEditor/CodeEditSourceEditor/CodeEditSourceEditor.swift
@@ -250,7 +250,7 @@ public struct CodeEditSourceEditor: NSViewControllerRepresentable {
 
         // **START: Text Content Update Logic**
         if !context.coordinator.isUpdateFromTextView { // Only update if user is NOT editing
-            DispatchQueue.main.async {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 switch text {
                 case .binding(let binding):
                     // Update text content from binding in updateNSViewController
